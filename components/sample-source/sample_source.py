8b5b68c861449a7d37630daa15b51b0bc91bd216
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-03-22 16:35:46 +0200

    Add MultiLine graph statistics reporting example

--- components/sample-source/sample_source.py
+++ components/sample-source/sample_source.py
@@ -1,5 +1,8 @@
+import time
+
 from parallelm.components import ConnectableComponent
 from parallelm.mlops import mlops
+from parallelm.mlops.stats.multi_line_graph import MultiLineGraph
 
 
 class SampleSource(ConnectableComponent):
@@ -9,6 +12,23 @@ class SampleSource(ConnectableComponent):
 
     def _materialize(self, parent_data_objs, user_data):
         str_value = self._params.get('value', "default-string-value")
+        self._demo_stats()
+        return [str_value]
+
+    def _demo_stats(self):
+        self._demo_line_graph()
+        self._demo_multi_line_graph()
+
+    def _demo_line_graph(self):
+        self._logger.info("Demo line graph ...")
         for x in range(100):
             mlops.set_stat("Sample line graph", x + 10 if x % 2 == 0 else x - 1)
-        return [str_value]
+        self._logger.info("Demo line graph done!")
+
+    def _demo_multi_line_graph(self):
+        self._logger.info("Demo multi-line graph ...")
+        mlt = MultiLineGraph().name("Multi-line graph example").labels(["l1", "l2", "l3"])
+        for x in range(100):
+            mlt.data([11 + x, 23 - x, 0.5 * x])
+            mlops.set_stat(mlt)
+        self._logger.info("Demo multi-line done!")
