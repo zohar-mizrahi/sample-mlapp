85a4dd8d871edf919fb00975cef847064300d925
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-03-22 16:23:28 +0200

    Add LineGraph statistics reporting example

--- components/sample-source/sample_source.py
+++ components/sample-source/sample_source.py
@@ -1,4 +1,5 @@
 from parallelm.components import ConnectableComponent
+from parallelm.mlops import mlops
 
 
 class SampleSource(ConnectableComponent):
@@ -8,4 +9,6 @@ class SampleSource(ConnectableComponent):
 
     def _materialize(self, parent_data_objs, user_data):
         str_value = self._params.get('value', "default-string-value")
+        for x in range(100):
+            mlops.set_stat("Sample line graph", x + 10 if x % 2 == 0 else x - 1)
         return [str_value]
